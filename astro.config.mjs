be5c5a7600acddbaa8494694bbbb607a6c281de5
kachaMukabe <kachamukabe@gmail.com>
2024-02-06 18:10:36 +0200

    Updated to latest versoin of astro, added vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,24 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-import prefetch from '@astrojs/prefetch';
+import prefetch from "@astrojs/prefetch";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://kachamukabe.com',
+  site: "https://kachamukabe.com",
+  output: "server",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
   integrations: [tailwind(), sitemap(), prefetch({ throttle: 3 })],
 });
